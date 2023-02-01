df444332b44a17b0f5b41da21b0bf0a7311574cf
Alexy Martinet <amlds.dev@gmail.com>
2023-02-01 12:13:56 +0100

    add roots and create pages

--- client/src/Root.tsx
+++ client/src/Root.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './views/Home';
+import Admin from './views/Admin';
 import NotFound from './views/NotFound';
 
 const Root = () => {
@@ -9,6 +10,7 @@ const Root = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
