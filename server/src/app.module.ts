d63d6828f170b598f5c9378d77ba4cff9b668c21
Alexy Martinet <amlds.dev@gmail.com>
2023-02-04 16:21:49 +0100

    change

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -9,7 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       host: process.env.DATABASE_HOST,
       port: 5432,
       username: process.env.DATABASE_USERNAME,
-      password: 'e59496c3',
+      password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
