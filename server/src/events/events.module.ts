5e5d037ce88191a3e1ac18101f31563b0ab502af
Alexy Martinet <amlds.dev@gmail.com>
2023-02-17 14:32:39 +0100

    modify server

--- server/src/events/events.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Event } from '../entity/event.entity';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([Event])],
-  exports: [TypeOrmModule],
-})
-export class EventsModule {}
